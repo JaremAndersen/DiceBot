815fd67a5822de0ed933e620d6006554f97f6cc1
JaremPDQ <jarem.andersen@pdq.com>
2021-08-05 21:00:06 -0600

    mostly memes

--- lib/telle_bot.ex
+++ lib/telle_bot.ex
@@ -27,6 +27,7 @@ defmodule TelleBot.MessageTick do
   def respond_to_messages([%{message: nil} | _tail]), do: nil
 
   def respond_to_messages([head | tail]) do
+    IO.inspect(head)
     spawn(fn -> respond(head.message.chat.id, head.message.text) end)
     respond_to_messages(tail)
   end
@@ -34,21 +35,49 @@ defmodule TelleBot.MessageTick do
   def respond(_id, nil), do: nil
 
   def respond(id, text) do
-    valid = Regex.run(~r/^\/r (\d+)[dD](\d+)$/, text)  |> Enum.map(&Integer.parse/1) |> Enum.filter(fn val -> val != :error end) |> IO.inspect()
+    valid =
+      Regex.run(~r/^\/r (\d+)[dD](\d+)$/, text)
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.filter(fn val -> val != :error end)
+      |> IO.inspect()
 
     case valid do
       nil ->
         Nadia.send_message(id, "Huh?")
+      [{69, _}, {69, _}] ->
+        Nadia.send_sticker(
+          id,
+          "CAACAgIAAxkBAAIBXmEMcDrKtJLqKgyyU0ewWz0CrV-VAAJiAQACUomRI3OAqJJmT9VtIAQ"
+        )
 
-      [{ number,_}, {size,_}] when number <= 69420 ->
-        dice = roll(number,size)
+      [{420, _}, {69, _}] ->
+        Nadia.send_sticker(
+          id,
+          "CAACAgIAAxkBAAIBV2EMbvjiuySDSJY4d58r9Vl3_JikAAJeAANSiZEj_nGk1T5n2YQgBA"
+        )
+
+        [{666, _}, {666, _}] ->
+          Nadia.send_sticker(
+            id,
+            "CAACAgIAAxkBAAIBhWEMeX4EL-9KsNP_F32OxZf81jJbAAKmAAP3AsgPqwzk86kqxlggBA"
+          )
+
+      [{number, _}, {size, _}] when number <= 69420 ->
+        dice = roll(number, size)
         prettyDice = String.replace("#{inspect(dice)}", "[", "") |> String.replace("]", "")
+
         total = Enum.sum(dice)
-        output = "You rolled a #{total}\n#{prettyDice}"
+        low = Enum.min(dice)
+        high = Enum.max(dice)
+        mean = total / length(dice)
+
+        output =
+          "You rolled a #{total}\nHigh: #{high} \nLow: #{low} \nAverage: #{mean} \n#{prettyDice}"
+
         Nadia.send_message(id, output)
 
       _ ->
-        Nadia.send_message(id, "🖕")
+        Nadia.send_sticker(id, "CAACAgIAAxkBAAIBX2EMcRp_Ki8iUzHAQAa_dqxYR5kXAAJ8AwACbbBCAwPPVZFWz3BMIAQ")
     end
   end
 
@@ -76,7 +105,6 @@ defmodule TelleBot.MessageTick do
   end
 
   def handle_info(:poll, offset) do
-    Logger.debug("Polling")
     next_offset = update(offset + 1) || 0
     schedule_poll()
     {:noreply, next_offset}
